d5ea62f4b4104b59b87bb5ed5a34abb00cd0d3a1
AnastasiaGritsuk <natikana_@mail.ru>
2020-07-12 13:19:42 +0300

    todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { TodoForm } from "./components/TodoForm";
+import { TodoList } from "./components/TodoList";
+import { ITodo } from "./interfaces";
+
+declare var confirm: (question: string) => boolean;
 
 const App: React.FC = () => {
-  return <div></div>;
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const addHandler = (title: string) => {
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false,
+    };
+    //setTodos([newTodo, ...todos]);
+    setTodos((prev) => [newTodo, ...prev]);
+  };
+
+  const toggleHandler = (id: number) => {
+    setTodos((prev) =>
+      prev.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+
+        return todo;
+      })
+    );
+  };
+
+  const removeHandler = (id: number) => {
+    const shouldRemove = confirm("Are you sure to delete the item? ");
+    if (shouldRemove) {
+      setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    }
+  };
+
+  return (
+    // React fragment
+    <>
+      <Navbar />
+      <div className="container">
+        <TodoForm onAdd={addHandler} />
+
+        <TodoList
+          todos={todos}
+          onToggle={toggleHandler}
+          onRemove={removeHandler}
+        />
+      </div>
+    </>
+  );
 };
 
 export default App;
